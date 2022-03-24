de8a23013f604b9f22097abe72fef4445cf0da8e
Gregory Schofield <greg.c.schofield@gmail.com>
2022-03-24 16:25:31 -0400

    Implement down command
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- compose/lib/compose/commands/down.rb
+++ compose/lib/compose/commands/down.rb
@@ -3,7 +3,22 @@ module Compose
     class Down < Base
       def exec
         each_svc do |name, defn|
-          hab_test("svc unload", nil, defn["pkg"])
+          load_line = "Unloading #{name.ljust(@name_offset)}  ... "
+          print load_line
+          options = []
+          _exitcode, stdout, stderr = hab(
+            :svc, 
+            {
+              sub_command: "unload",
+              options: options.append("--remote-sup=#{@remote_sup}"),
+              verbose: @verbose
+            }, 
+            defn["pkg"]
+          )
+          not_loaded = "\r#{name} is not loaded"
+          (load_line.size - not_loaded.size).times {|_| not_loaded.concat(" ") }
+          print not_loaded + "\n" if stderr =~ /Service #{Regexp.quote(defn["pkg"])} not loaded/
+          print Paint["done\n", :green] if stderr.eql?("")
         end
       end
     end
