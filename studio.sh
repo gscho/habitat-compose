51eff34b763bbfd33735ab34822f9313a8a4488e
Gregory Schofield <greg.c.schofield@gmail.com>
2022-04-08 12:45:45 -0400

    Set the HAB_CTL_SECRET on startup
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- studio.sh
+++ studio.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-HAB_DOCKER_OPTS="-p 9632:9632 -v $(pwd)/default:/hab/sup/default" hab studio enter
+HAB_CTL_SECRET=$(cat ~/.hab/etc/cli.toml | grep "ctl_secret" | cut -f2 -d '"') HAB_DOCKER_OPTS="-p 9632:9632 -v $(pwd)/default:/hab/sup/default" hab studio enter
