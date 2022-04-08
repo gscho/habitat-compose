d61b8fa25b987658e2b163b4ced5e3bc322c5a55
Gregory Schofield <greg.c.schofield@gmail.com>
2022-04-08 12:46:05 -0400

    Add starting and stopping
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- compose/lib/compose/screen_output.rb
+++ compose/lib/compose/screen_output.rb
@@ -6,10 +6,21 @@ module Compose
       load_line = "Loading #{name.ljust(offset)}    ... "
       print load_line
       _exitcode, stdout, stderr = yield
-      
+
       up_to_date = "\r#{name} is up-to-date"
-      (load_line.size - up_to_date.size).times {|_| up_to_date.concat(" ") }
+      (load_line.size - up_to_date.size).times { |_| up_to_date.concat(" ") }
       print up_to_date + "\n" if stderr =~ /Service already loaded/
+      print Paint["done\n", :green] if stderr.eql?("")
+    end
+
+    def print_starting(name, offset)
+      load_line = "Starting #{name.ljust(offset)}    ... "
+      print load_line
+      _exitcode, stdout, stderr = yield
+      if stderr != ""
+        print Paint["error\n", :red] 
+        puts stderr
+      end
       print Paint["done\n", :green] if stderr.eql?("")
     end
 
@@ -17,10 +28,21 @@ module Compose
       unload_line = "Unloading #{name.ljust(offset)}  ... "
       print unload_line
       _exitcode, stdout, stderr = yield
-      
+
       not_loaded = "\r#{name} is not loaded"
-      (unload_line.size - not_loaded.size).times {|_| not_loaded.concat(" ") }
+      (unload_line.size - not_loaded.size).times { |_| not_loaded.concat(" ") }
       print not_loaded + "\n" if stderr =~ /Service #{Regexp.quote(pkg)} not loaded/
+      print Paint["done\n", :green] if stderr.eql?("")
+    end
+
+    def print_stoping(name, offset)
+      load_line = "Stopping #{name.ljust(offset)}    ... "
+      print load_line
+      _exitcode, stdout, stderr = yield
+      if stderr != ""
+        print Paint["error\n", :red] 
+        puts stderr
+      end
       print Paint["done\n", :green] if stderr.eql?("")
     end
   end
