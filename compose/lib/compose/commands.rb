d88c33e39da6c56c79aa8cf8fee46a5fb05d1302
Gregory Schofield <greg.c.schofield@gmail.com>
2022-04-08 12:44:35 -0400

    Remove restart class
    Signed-off-by: Gregory Schofield <greg.c.schofield@gmail.com>

--- compose/lib/compose/commands.rb
+++ compose/lib/compose/commands.rb
@@ -4,12 +4,10 @@ require_relative "commands/base"
 require_relative "commands/build"
 require_relative "commands/down"
 require_relative "commands/logs"
-require_relative "commands/restart"
 require_relative "commands/start"
 require_relative "commands/status"
 require_relative "commands/stop"
 require_relative "commands/up"
-
 
 module Compose
   module Commands
